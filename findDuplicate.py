43be0935100958bf0906e450106a1d9dcf4ed2b2
ironbreakdev <mauro.spezzaferro@gmail.com>
2020-11-22 10:31:25 +0100

    script hash dusplicate remove

---
+++ findDuplicate.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+import os,hashlib,sys
+
+dict_file = {}
+unique_file = []
+
+if ( len(sys.argv) < 2 ):
+    print("Attenzione devi inserire il path da analizzare")
+    exit()
+
+if not os.path.exists(sys.argv[1]):
+    print("Attenzione, il path inserito non è una directory")
+    exit()
+
+path = sys.argv[1]
+dir = os.listdir(path)
+
+print("-"*35)
+print("All'interno del path: "+path+": ci sono "+str(len(dir))+" files")
+for file in dir:    
+    with open(path+"/"+file,"rb") as f: 
+        bytes = f.read()
+        hash = hashlib.md5(bytes).hexdigest()
+        dict_file[hash] = file
+
+if str(len(dict_file)) == str(len(dir)):
+    print("Di questi non esistono file duplicati")
+    exit()
+else:
+    print("Di questi solo "+str(len(dict_file))+" non hanno duplicati e sono: \n")
+    for key,value in dict_file.items(): 
+        print(value)
+
+    esito = input("\nVuoi eliminare tutti i file che sono duplicati? [s/n]: ")
+    while esito.lower() != 's' or esito.lower() != 'n':
+        if esito.lower() == 's':
+            for file in os.listdir(path): 
+                if file not in dict_file.values():
+                    os.remove(path+"/"+file)
+            break
+        elif esito.lower() == 'n':
+            exit()
+        else:
+            print("Attenzione, carattere non riconosciuto")
+            esito = input("\nVuoi eliminare tutti i file che sono duplicati? [s/n]: ")
